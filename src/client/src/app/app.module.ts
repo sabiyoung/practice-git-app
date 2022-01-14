c7c4fa4bbb4c08452667d88576465a425b4d246b
saba <sabera@alphaworks.tech>
2022-01-14 13:39:52 -0800

    feat(component) generate app component

---
+++ src/client/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AddTodoComponent } from './pages/add-todo/add-todo.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AddTodoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
