f86afaa075b43f57cae00f322622ba4f07fdfeb4
saba <sabera@alphaworks.tech>
2022-01-14 16:59:49 -0800

    feat(server): connect to mongoose, add create todo & get todos

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,10 +1,60 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
-
+import {TodoModel } from './schemas/todo.schama.js'
 const app = express();
 const port = 3000
 
-app.listen(port, function() {
-console.log('server is running at', port)
+
+const PORT = process.env.PORT || 3000;
+mongoose
+.connect('mongodb://localhost:27017/practiceGitApp')
+  .then(() => {
+    console.log("Connected to DB Successfully");
+  })
+
+
+
+
+  app.use(cors({
+      credentials: true,
+      origin: ['http://localhost:3000', 'http://localhost:4200', 'http://localhost:3501', 'http://localhost:8080']
+  }));
+  app.use(express.json());
+
+
+
+  app.post("/create-todo", function(req, res) {
+    const {text} = req.body
+    const todo = new TodoModel({
+    
+      text
+    });
+todo
+    .save()
+    .then((data) => {
+      res.json((data))
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(501);
+      res.json({errors: err})
+    })
+  })
+  app.get("/todos", function(req, res) {
+    TodoModel.find()
+    .then((data) => res.json({data}))
+    .catch((err) => {
+      res.status(501);
+      res.json({ errors: err });
+    });
+  })
+
+
+
+
+
+
+app.listen(PORT, function() {
+console.log('server is running at', PORT)
 })
